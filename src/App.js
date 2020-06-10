275a3ba11e51a3da4e978125bafcd41d5dde89fd
habeelwali <habeelwal3@gmail.com>
2020-06-10 04:06:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
      <h1><Total total='220,892,340'/></h1> 
 
-      <h1><Pak provance="kpk" population="35.53 million "/></h1>
+      <h1><Pak provance="kpk" population="35.54 million "/></h1>
       <h1><Pak provance="punjab" population="110,012,442 "/></h1>
       <h1><Pak provance="sindh" population="47.89 million "/></h1>
       <h1><Pak provance="Balochistan" population="12.34 million"/></h1>
