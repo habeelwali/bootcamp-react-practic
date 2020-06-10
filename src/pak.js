4fa67569adbcdab2090b8cfcf6f7f42a06a0b3ce
habeelwali <habeelwal3@gmail.com>
2020-06-10 02:33:36 +0000

    Initialize project using Create React App

---
+++ src/pak.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Pak(props){
+    return(
+    <div>
+     
+      
+    <h3>population of {props.provance} is {props.population}</h3>
+    
+    </div>
+    );
+  }
+  export default Pak;
---
+++ src/pak2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Total(props){
+    return(
+    <div>
+     
+      
+    <h3>total population of pakistan is  {props.total}</h3>
+    
+    </div>
+    );
+  }
+  export default Total;
